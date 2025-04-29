c29e3891e434a73cb29d74bad541f961a20a0d61
Success Olugbenga <preshdivine372@gmail.com>
2025-04-29 01:19:22 -0700

    finished the form

---
+++ script.js
@@ -0,0 +1,44 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+// if we need pagination
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+},
+
+// Navigation arrows
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+}, 
+// Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
